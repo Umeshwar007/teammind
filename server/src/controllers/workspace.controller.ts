ed68a1d2cb589c28d791e1721905d2ff8249ae26
Umeshwar007 <151039459+Umeshwar007@users.noreply.github.com>
2026-06-23 18:23:00 +0530

    rolebased access created

--- server/src/controllers/workspace.controller.ts
+++ server/src/controllers/workspace.controller.ts
@@ -27,3 +27,38 @@ export async function createWorkspace(req: AuthRequest, res: Response) {
         return res.status(500).json({ message: "Internal server error" });
     }
 }
+
+export async function joinWorkspace(req: AuthRequest, res: Response) {
+    try{
+        const workspaceId = Array.isArray(req.params.workspaceId) ? req.params.workspaceId[0] : req.params.workspaceId;
+
+
+
+        const userId = req.user!.userId;
+        const workspace = await prisma.workspace.findUnique({
+            where: { id: workspaceId }
+        });
+        if(!workspace){
+            return res.status(404).json({ message: "Workspace not found" });
+        }
+        const existing = await prisma.membership.findUnique({
+            where: {
+                userId_workspaceId: { userId, workspaceId}},
+            });
+            if(existing){
+                return res.status(400).json({ message: "User is already a member of this workspace" });
+            }
+            const membership = await prisma.membership.create({
+                data: {
+                    userId,
+                    workspaceId,
+                    role: "MEMBER",
+                },
+            });
+            return res.status(201).json({membership});
+    }
+    catch (error) {
+        console.error("Error joining workspace:", error);
+        return res.status(500).json({ message: "Internal server error" });
+    }
+}
--- server/src/routes/workspace.routes.ts
+++ server/src/routes/workspace.routes.ts
@@ -1,11 +1,12 @@
 import {Router} from 'express';
-import {createWorkspace} from '../controllers/workspace.controller';
+import {createWorkspace,joinWorkspace} from '../controllers/workspace.controller';
 import {authenticate} from '../middleware/auth.middleware';
 import channelRoutes from './channel.routes';
 
 const router = Router();
 
 router.post('/', authenticate, createWorkspace);
+router.post('/:workspaceId/join', authenticate, joinWorkspace);
 router.use('/:workspaceId/channels', channelRoutes);
 
 export default router;
